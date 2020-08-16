23cf00fe1c35396309bcaa4848b90d7f4c1e7c25
fkropfhamer <fabian.kropfhamer@gmail.com>
2020-08-16 19:15:59 +0200

    cleanup

--- src/mnist/main.js
+++ src/mnist/main.js
@@ -1,5 +1,6 @@
 import * as tf from '@tensorflow/tfjs';
-import { chunkArray, indexOfMax } from '../util/util';
+import { indexOfMax } from '../util/util';
+import { normalize } from './normalize';
 import Draw from './draw';
 
 async function loadModel() {
@@ -15,93 +16,6 @@ async function predict(model, inputTensor) {
     const predictedNumber = indexOfMax(predictionArray[0]);
     
     return predictedNumber;
-}
-
-function normalize(ctx) {
-    const boundBox = getMinBoundBox(ctx);
-
-    const boundBoxHeight = boundBox.maxPoint.y - boundBox.minPoint.y;
-    const boundBoxWidth = boundBox.maxPoint.x - boundBox.minPoint.x;
-
-    const x = boundBoxHeight > boundBoxWidth ? boundBoxHeight : boundBoxWidth;
-
-    const normalizedCanvas = document.createElement("canvas");
-    normalizedCanvas.width = 20;
-    normalizedCanvas.height = 20;
-    const context = normalizedCanvas.getContext("2d");
-
-    context.drawImage(ctx.canvas, boundBox.minPoint.x, boundBox.minPoint.y, x, x, 0, 0, 20, 20);
-
-    const pm = Draw.getPixelMatrix(context);
-
-    const centerOfMass = getCenterOfMass(pm);
-
-    const normalizedCanvas2 = document.createElement("canvas");
-
-    normalizedCanvas2.width = 28;
-    normalizedCanvas2.height = 28;
-    const normalizedContext = normalizedCanvas2.getContext("2d");
-
-    const halfSize = 28 / 2;
-
-    normalizedContext.drawImage(normalizedCanvas, halfSize - centerOfMass.x, halfSize - centerOfMass.y);
-
-    return normalizedContext;
-}
-
-function getCenterOfMass(pixelMatrix) {
-    let numPixels = 0;
-    let sumX = 0;
-    let sumY = 0;
-    pixelMatrix.forEach((ys, y) => {
-        ys.forEach((pixel, x) => {
-            const weight = (pixel / 255)
-            sumX += (x * weight);
-            sumY += (y * weight);
-            numPixels += weight;
-        })
-    })
-
-    const x = sumX / numPixels;
-    const y = sumY / numPixels;
-
-    return { x, y };
-}
-
-function getMinBoundBox(ctx) {
-    const height = ctx.canvas.height;
-    const width = ctx.canvas.width;
-
-    const pixelArray = Draw.getGreyScalePixelArray(ctx);
-    const pixelMatrix = chunkArray(pixelArray, width);
-
-    let minY = Infinity;
-    let minX = Infinity;
-    let maxY = -Infinity;
-    let maxX = -Infinity;
-
-
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < height; x++) {
-            const pixel = pixelMatrix[y][x];
-            if (pixel > 0) {
-                if (minX > x) {
-                    minX = x;
-                }
-                if (minY > y) {
-                    minY = y;
-                }
-                if (maxX < x) {
-                    maxX = x;
-                }
-                if (maxY < y) {
-                    maxY = y;
-                }
-            } 
-        }
-    } 
-
-    return { minPoint: { x: minX, y: minY }, maxPoint: { x: maxX, y: maxY } };
 }
 
 async function main() {
@@ -123,18 +37,13 @@ async function main() {
     predictButton.onclick = async () => {
         const normalizedContext = normalize(draw.ctx);
 
-        console.log(normalizedContext);
-
         const pixelArray = Array.from(Draw.getGreyScalePixelArray(normalizedContext));
-
         const inputTensor = tf.tensor(pixelArray)
 
         const predicition = await predict(model, inputTensor);
-
-        console.log('predicted', predicition);
 
         outputElement.innerHTML = `predicted ${predicition}`;
     }
 }
 
-main().then(() => console.log("finished"));
+main();
